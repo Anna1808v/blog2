49430e0e1f0932a398b7547a642b0c06ae314817
Anna1808v <voskoboyeva18@gmail.com>
2023-04-04 19:35:27 +0300

    bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,21 +3,36 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Document</title>
 </head>
 <body>
-    <div>
-        <nav>
-            <ul>
-                <li><a href="{{ route('main.index') }}">Main</a></li>
-                <li><a href="{{ route('contact.index') }}">Contacts</a></li>
-                <li><a href="{{ route('comment.index') }}">Comments</a></li>
-                <li><a href="{{ route('about.index') }}">About</a></li>
-            </ul>
-        </nav>
+<div class="container">
+    <div class="row">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('main.index') }}">Main</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('comment.index') }}">Comments</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('about.index') }}">About</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </div>
     <div>
         @yield('content')
     </div>
+</div>
 </body>
 </html>
