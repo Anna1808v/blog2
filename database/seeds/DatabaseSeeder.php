b68393dca829e3c2f2f42b326e1079cf06da2b92
Anna1808v <voskoboyeva18@gmail.com>
2023-06-03 17:49:50 +0300

    CategoryFactory, TagFactory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Tag;
 use App\Comment;
+use App\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,13 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
 
-        factory(Comment::class)->times(100)->create();
+        factory(Category::class)->times(80)->create();
+        $tags = factory(Tag::class)->times(200)->create();
+        $comments = factory(Comment::class)->times(100)->create();    
+
+        foreach($comments as $comment){
+            $tagsIds = $tags->random(5)->pluck('id');
+            $comment->tags()->attach($tagsIds);
+        }
     }
 }
